a163b813df514d92de44906a079913d84d35bba0
alumno648367 <alumno.648367@ies-azarquiel.es>
2024-05-03 13:44:52 +0200

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+/*hazme un script */
+window.onload = function(){
+    document.querySelector('header').style.color = 'white';
+    document.querySelector('main').style.color = 'white';
+    document.querySelector('footer').style.color = 'white';
+}
